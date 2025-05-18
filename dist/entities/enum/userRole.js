b8ccd047acb59f2c3ba32f51a2faf020fa9d098f
AbhinavGupta <bengupta786@gmail.com>
2025-05-19 01:14:01 +0530

    Remove dist folder from repository

--- dist/entities/enum/userRole.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRole = void 0;
-var UserRole;
-(function (UserRole) {
-    UserRole["STUDENT"] = "student";
-    UserRole["SOCIETY_ADMIN"] = "society_admin";
-})(UserRole || (exports.UserRole = UserRole = {}));
